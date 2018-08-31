11ccc21ebaa21475cddce3e4efc0ce4360862a30
Rane M Kaseman <raykman@comcast.net>
2018-08-31 00:45:16 -0600

    0045

--- climazon_db.sql
+++ climazon_db.sql
@@ -5,15 +5,23 @@ USE climazon_db;
 
 CREATE TABLE inventory (
   id INTEGER NOT NULL AUTO_INCREMENT,
-  item VARCHAR(45) NULL,
-  itemValue INTEGER(UNSIGNED) NULL,
+  item VARCHAR(40) NULL,
+  department VARCHAR(40),
+  price MONEY NOT NULL,
+  stock INTEGER NOT NULL,
   PRIMARY KEY (id)
 );
 
-INSERT INTO auctions (item, itemValue)
+INSERT INTO inventory (item, department, price, stock)
 VALUES
-("Renewable Energy", 1001),
-("Better Education", 1002),
-("Rational Healthcare", 1003),
-("Trip to Mars", 1004);
+("Renewable Energy", "Utopian Dreams", 1000, 10000),
+("Better Education", "Utopian Dreams", 1500, 5000),
+("Rational Healthcare", "Utopian Dreams", 1750, 750),
+("Critical Thinking Skills", "Utopian Dreams", 2000, 500),
+("Vacation in Orbit", "Science Fiction Dreams", 1000000, 100),
+("Vacation on Luna", "Science Fiction Dreams", 4500000, 10),
+("Trip to Mars", "Science Fiction Dreams", 1250000, 5),
+("Trip to Saturn", "Science Fiction Dreams", 2000000, 4),
+("Trip to Andromeda", "Science Fiction Dreams", 5000000, 2),
+("One-way to the Galactic Core's Black Hole", "Science Fiction Dreams", 10000000, 1);
 
